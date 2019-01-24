35c9a452986c4f28e9fa54e03e84be039be6f83d
brunobengo <brunobengo@gmail.com>
2019-01-23 23:22:55 -0200

    Melhoria no tipo do teclado utilizado para leitura dos valores.

--- AlcoolOuGasolina/ViewController.swift
+++ AlcoolOuGasolina/ViewController.swift
@@ -10,11 +10,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultadoLegenda: UILabel!    
+    @IBOutlet weak var precoGasolina: UITextField!
+    @IBOutlet weak var precoAlcool: UITextField!
+   
+    @IBAction func calcularCombustivel(_ sender: Any) {
+        if let letPrecoAlcool = precoAlcool.text {
+            if let letPrecoGasolina = precoGasolina.text {
+                let validaCampos = self.validaCampos(precoAlcool: letPrecoAlcool,
+                                                     precoGasolina: letPrecoGasolina)
+                if validaCampos {
+                    calcularMelhorPreco(precoAlcool: letPrecoAlcool, precoGasolina: letPrecoGasolina)
+                }else{
+                    resultadoLegenda.text = "Digite os preços para calcular!"
+                }
+                
+            }
+        }
+        
+    }
+    
+    func calcularMelhorPreco(precoAlcool: String, precoGasolina: String) -> Bool{
+        if let valorAlcool = Double(precoAlcool){
+            if let valorGasolina = Double(precoGasolina){
+                let resultadoPreco = valorAlcool / valorGasolina
+                if resultadoPreco >= 0.7 {
+                    self.resultadoLegenda.text = "Melhor utilizar gasolina!"
+                }else{
+                    self.resultadoLegenda.text = "Melhor utilizar Alcool!"
+                    
+                }
+            }
+        }
+        return true
+    }
+    
+    func validaCampos(precoAlcool: String, precoGasolina: String) -> Bool{
+        
+        return !precoAlcool.isEmpty && !precoGasolina.isEmpty
+        
+//        var camposValidados = true
+//
+//        if precoAlcool.isEmpty {
+//            camposValidados = false
+//        }else if precoGasolina.isEmpty {
+//            camposValidados = false
+//        }
+//
+//        return camposValidados
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    
 
 
+   
 }
 
